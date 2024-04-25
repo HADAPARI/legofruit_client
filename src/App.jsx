066f2a16214766ffae0434a308467d2da6373179
[Mahanitia] <[ramiandrizakamahanitia@gmail.com]>
2024-04-25 11:15:52 +0300

    finalisation details modification du profile

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ErrorPage from "./pages/ErrorPage";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
-import Profile from "./pages/Profile";
+import Profile from "./pages/Profiles";
 // import { decrement, increment } from "./redux/reducers/counterSlice";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
